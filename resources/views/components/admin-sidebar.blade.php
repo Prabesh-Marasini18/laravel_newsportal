790c504d7fc09ab99629c1c6da1e3a46641ee5c3
Prabesh-Marasini18 <prabeshmarasini18@gmail.com>
2025-08-25 16:45:42 +0545

    admin dashboard setup

---
+++ resources/views/components/admin-sidebar.blade.php
@@ -0,0 +1,29 @@
+<aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="{{ route('dashboard') }}"> <img alt="image" src="/assets/img/logo.png" class="header-logo" /> <span
+                class="logo-name">Prabesh Hub</span>
+            </a>
+          </div>
+          <ul class="sidebar-menu">
+            <li class="menu-header">Main</li>
+            <li class="dropdown active">
+              <a href="{{ route('dashboard') }}" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+            </li>
+
+
+             <li class="dropdown">
+              <a href="{{ route('dashboard') }}" class="nav-link"><i data-feather="briefcase"></i><span>Company</span></a>
+            </li>
+
+             <li class="dropdown">
+              <a href="{{ route('dashboard') }}" class="nav-link"><i data-feather="tag"></i><span>Category</span></a>
+            </li>
+
+             <li class="dropdown">
+              <a href="{{ route('dashboard') }}" class="nav-link"><i data-feather="book"></i><span>Article</span></a>
+            </li>
+
+
+
+          </ul>
+        </aside>
